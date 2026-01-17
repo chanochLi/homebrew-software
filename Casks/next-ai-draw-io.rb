e2090a37c035a4c10670ed94c9a2eaed89b9fb55
chanochLi <kujou@foxmail.com>
2026-01-18 00:36:07 +0800

    update to new profile

--- Casks/next-ai-draw-io.rb
+++ Casks/next-ai-draw-io.rb
@@ -2,7 +2,7 @@ cask "next-ai-draw-io" do
   arch arm: "arm64"
 
   version "0.4.10"
-  sha256 arm:   "0918f7f60c15d9ba3cbd0adb74598661e36aa189c1ba43a65c84bff970dd94b1"
+  sha256 arm: "0918f7f60c15d9ba3cbd0adb74598661e36aa189c1ba43a65c84bff970dd94b1"
 
   url "https://github.com/DayuanJiang/next-ai-draw-io/releases/download/v#{version}/Next-AI-Draw.io-#{version}#{arch == "arm64" ? "-arm64" : ""}.dmg",
       verified: "github.com/DayuanJiang/next-ai-draw-io/"
@@ -16,7 +16,6 @@ cask "next-ai-draw-io" do
   end
 
   auto_updates true
-
   depends_on macos: ">= :monterey"
 
   app "Next AI Draw.io.app"
@@ -31,20 +30,4 @@ cask "next-ai-draw-io" do
       exec '#{appdir}/Next AI Draw.io.app/Contents/MacOS/Next AI Draw.io' "$@"
     EOS
   end
-  caveats <<~EOS
-  This app is not code-signed/notarized. macOS may show security warnings.
-
-  If you see “App is damaged”, you can remove the quarantine attribute:
-
-    sudo xattr -rd com.apple.quarantine "#{appdir}/Next AI Draw.io.app"
-
-  the command is to be executed by homebrew
-EOS
-
-postflight do
-  system_command "/usr/bin/xattr",
-                 args: ["-rd", "com.apple.quarantine", "#{appdir}/Next AI Draw.io.app"],
-                 sudo: true
-end
-
 end

f77920d4265672aea2680225ce597398d413ba81
chanochLi <kujou@foxmail.com>
2026-01-18 00:49:22 +0800

    fix reverse to 0.4.9

--- Casks/next-ai-draw-io.rb
+++ Casks/next-ai-draw-io.rb
@@ -2,7 +2,7 @@ cask "next-ai-draw-io" do
   arch arm: "arm64"
 
   version "0.4.9"
-  sha256 arm: "sha256:c7eeb700b15102d78172a6178e849477181382cdee11e256176002d51bfab187"
+  sha256 arm: "c7eeb700b15102d78172a6178e849477181382cdee11e256176002d51bfab187"
 
   url "https://github.com/DayuanJiang/next-ai-draw-io/releases/download/v#{version}/Next-AI-Draw.io-#{version}#{arch == "arm64" ? "-arm64" : ""}.dmg",
       verified: "github.com/DayuanJiang/next-ai-draw-io/"

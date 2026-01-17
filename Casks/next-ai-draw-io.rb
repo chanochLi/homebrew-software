7429b2183b883c8a64bbb32d54af972b6f7e5823
chanochLi <kujou@foxmail.com>
2026-01-18 00:28:55 +0800

    update sha256

--- Casks/next-ai-draw-io.rb
+++ Casks/next-ai-draw-io.rb
@@ -2,7 +2,7 @@ cask "next-ai-draw-io" do
   arch arm: "arm64"
 
   version "0.4.10"
-  sha256 arm:   "sha256:0918f7f60c15d9ba3cbd0adb74598661e36aa189c1ba43a65c84bff970dd94b1"
+  sha256 arm:   "0918f7f60c15d9ba3cbd0adb74598661e36aa189c1ba43a65c84bff970dd94b1"
 
   url "https://github.com/DayuanJiang/next-ai-draw-io/releases/download/v#{version}/Next-AI-Draw.io-#{version}#{arch == "arm64" ? "-arm64" : ""}.dmg",
       verified: "github.com/DayuanJiang/next-ai-draw-io/"
